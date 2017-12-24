baa33fb55224199a1d111d26657f83e0cac77d84
melwaraki <mirage299@gmail.com>
2017-12-25 01:21:16 +0300

    Initial Commit

---
+++ Burst DissertationTests/Burst_DissertationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Burst_DissertationTests.swift
+//  Burst DissertationTests
+//
+//  Created by Marawan Alwaraki on 25/12/2017.
+//  Copyright © 2017 Marawan Alwaraki. All rights reserved.
+//
+
+import XCTest
+@testable import Burst_Dissertation
+
+class Burst_DissertationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
